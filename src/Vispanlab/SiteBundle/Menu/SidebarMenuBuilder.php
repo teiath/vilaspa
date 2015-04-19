07765946a7fad7c978df4e9c7af2b20e11f747cd
Dimosthenis Nikoudis <dnna@dnna.gr>
2015-04-20 01:58:50 +0300

    Allow only one item to be expanded at a time in left hand menu

--- src/Vispanlab/SiteBundle/Menu/SidebarMenuBuilder.php
+++ src/Vispanlab/SiteBundle/Menu/SidebarMenuBuilder.php
@@ -13,13 +13,16 @@ class SidebarMenuBuilder
      */
     private $em;
 
+    private $securityContext;
+
     /**
      * @param FactoryInterface $factory
      */
-    public function __construct(FactoryInterface $factory, EntityManager $entityManager)
+    public function __construct(FactoryInterface $factory, EntityManager $entityManager, $securityContext)
     {
         $this->factory = $factory;
         $this->em = $entityManager;
+        $this->securityContext = $securityContext;
     }
 
     public function createSideMenu(Request $request)
@@ -31,6 +34,20 @@ class SidebarMenuBuilder
             $areaMenu = $menu->addChild($curArea->getName(), array('uri' => '#', 'attributes' => array('class' => 'home')));
             $areaMenu->addChild('Βιβλιοθήκη Εννοιών', array('route' => 'concept_library', 'routeParameters' => array('aoe' => $curArea->getUrl()), 'attributes' => array('class' => 'home')));
             $areaMenu->addChild('Εικονικές Ασκήσεις', array('route' => 'virtual_exercises', 'routeParameters' => array('aoe' => $curArea->getUrl()), 'attributes' => array('class' => 'home')));
+        }
+
+        return $menu;
+    }
+
+    public function createBottomMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+        if($this->securityContext->isGranted('ROLE_ADMIN') || $this->securityContext->isGranted('ROLE_AREA_ADMIN')) {
+            if(strpos($request->getRequestUri(), 'admin') === false) {
+                $menu->addChild('common.admin_env_link', array('route' => 'sonata_admin_dashboard', 'attributes' => array('class' => 'home')));
+            } else {
+                $menu->addChild('common.user_env_link', array('route' => 'home', 'attributes' => array('class' => 'home')));
+            }
         }
         $menu->addChild('Οδηγίες Χρήσης', array('uri' => '#docs', 'attributes' => array('class' => 'home')));
 
