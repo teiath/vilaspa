46a2d1c1fe78c4b89ad40d48e19c7039831ad5fe
Dimosthenis Nikoudis <dnna@dnna.gr>
2015-05-20 14:44:48 +0300

    Fix search redirect when in concept page

--- src/Vispanlab/SiteBundle/Entity/AreaOfExpertise.php
+++ src/Vispanlab/SiteBundle/Entity/AreaOfExpertise.php
@@ -94,8 +94,8 @@ class AreaOfExpertise {
     public function getSortedConcepts($locale) {
         $concepts = $this->getConcepts($locale)->getIterator();
         $concepts->uasort(function ($a, $b) use ($locale) {
-            $at = $this->stripGrAccent($a->getNameForLang($locale)->getText_formatted());
-            $bt = $this->stripGrAccent($b->getNameForLang($locale)->getText_formatted());
+            $at = $this->stripGrAccent(strip_tags($a->getNameForLang($locale)->getText_formatted()));
+            $bt = $this->stripGrAccent(strip_tags($b->getNameForLang($locale)->getText_formatted()));
             if($at == $bt) { return 0; }
             return ($at > $bt) ? 1 : -1;
         });
