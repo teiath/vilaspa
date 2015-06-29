22494fc1a688bfc07766acfe31270a993ed4d1b9
Dimosthenis Nikoudis <dnna@dnna.gr>
2015-06-29 15:34:59 +0300

    Added search to all pages

--- src/Vispanlab/SiteBundle/Controller/ConceptLibraryController.php
+++ src/Vispanlab/SiteBundle/Controller/ConceptLibraryController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use JMS\SecurityExtraBundle\Annotation\Secure;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Vispanlab\SiteBundle\Entity\AreaOfExpertise;
 use Vispanlab\SiteBundle\Entity\Concept;
@@ -33,5 +34,21 @@ class ConceptLibraryController extends Controller {
             'next' => $this->get('vispanlab.concept.service')->nextConcept($concept, $aoe, $this->getRequest()->getLocale()),
             'prev' => $this->get('vispanlab.concept.service')->prevConcept($concept, $aoe, $this->getRequest()->getLocale()),
         ));
+    }
+
+    /**
+     * @Route("/search_concepts", name="search_concepts")
+     * @Secure(roles="ROLE_USER")
+     */
+    public function searchConcepts() {
+        $locale = $this->getRequest()->getLocale();
+        $concepts = $this->container->get('doctrine')->getManager()->createQuery('SELECT c.id, def.text_formatted as value, aoe.url FROM Vispanlab\SiteBundle\Entity\Concept c JOIN c.name def WITH def.locale = \''.$locale.'\' JOIN c.areasofexpertise aoe WHERE def.text LIKE :searchTerm');
+        $concepts->setParameter('searchTerm', '%'.$this->getRequest()->get('q').'%');
+        $concepts = $concepts->execute();
+        $concepts = array_map(function($c) {
+            $c['value'] = str_replace('&amp;', '', strip_tags($c['value']));
+            return $c;
+        }, $concepts);
+        return new JsonResponse($concepts);
     }
 }
