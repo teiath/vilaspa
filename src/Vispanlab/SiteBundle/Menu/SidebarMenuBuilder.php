6ed3ce4840b37caf6d7cca6bbb85a496d4513b8c
Dimosthenis Nikoudis <dnna@dnna.gr>
2015-08-18 11:38:33 +0300

    Fixed tech guide link

--- src/Vispanlab/SiteBundle/Menu/SidebarMenuBuilder.php
+++ src/Vispanlab/SiteBundle/Menu/SidebarMenuBuilder.php
@@ -65,7 +65,7 @@ class SidebarMenuBuilder
         $menu = $this->factory->createItem('root');
         $menu->addChild('common.admin_guide', array('route' => 'admin_guide', 'attributes' => array('class' => 'home')));
         $menu->addChild('common.analytics', array('uri' => '#analytics', 'attributes' => array('class' => 'home')));
-        $menu->addChild('common.tech_guide', array('uri' => '#maintainers_guide', 'attributes' => array('class' => 'home')));
+        $menu->addChild('common.tech_guide', array('uri' => '/tech_guide.docx', 'attributes' => array('class' => 'home')));
         if($this->securityContext->isGranted('ROLE_ADMIN') || $this->securityContext->isGranted('ROLE_AREA_ADMIN')) {
             if(strpos($request->getRequestUri(), 'admin') === false) {
                 $menu->addChild('common.admin_env_link', array('route' => 'sonata_admin_dashboard', 'attributes' => array('class' => 'home')));
