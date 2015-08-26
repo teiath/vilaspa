0719a0bb560fc14e00aea499ab93f90c55446d7c
Dimosthenis Nikoudis <dnna@dnna.gr>
2015-08-26 12:09:14 +0300

    Remove hacky fix for comment bundle

--- src/Vispanlab/CommonBundle/VispanlabCommonBundle.php
+++ src/Vispanlab/CommonBundle/VispanlabCommonBundle.php
@@ -4,13 +4,11 @@ namespace Vispanlab\CommonBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Vispanlab\CommonBundle\DependencyInjection\Compiler\FixedCommentExtensionCompilerPass;
 
 class VispanlabCommonBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-        $container->addCompilerPass(new FixedCommentExtensionCompilerPass());
     }
 }
